1dafcafcc9174139ba953a9656df67d845512106
Pedro Machado <48498905+pedromchd@users.noreply.github.com>
2023-09-22 13:03:07 +0000

    Modificando a confirmação de inscrição

--- eventif/urls.py
+++ eventif/urls.py
@@ -17,12 +17,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from core.views import home
-from subscriptions.views import subscribe
+from subscriptions.views import subscribe, detail
 from contact.views import contact
 
 urlpatterns = [
     path('', home),
     path('inscricao/', subscribe),
+    path('inscricao/<int:pk>/', detail),
     path('contato/', contact),
     path('admin/', admin.site.urls),
 ]

168e9c169a5022b2aa05808989bdd9932291babb
Pedro Machado <48498905+pedromchd@users.noreply.github.com>
2023-10-27 12:29:59 +0000

    Capitalized name

--- subscriptions/tests/test_form_subscribe.py
+++ subscriptions/tests/test_form_subscribe.py
@@ -16,6 +16,10 @@ class SubscriptionFormTest(TestCase):
         form = self.make_validated_form(cpf='123456')
         self.assertFormErrorCode(form, 'cpf', 'length')
 
+    def test_name_must_be_capitalized(self):
+        form = self.make_validated_form(name='PEDRO machado')
+        self.assertEqual('Pedro Machado', form.cleaned_data['name'])
+
     def assertFormErrorCode(self, form, field, code):
         errors = form.errors.as_data()
         error_list = errors[field]

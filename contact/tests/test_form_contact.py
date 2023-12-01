6b8fa55c853b31733575f90f1fac314a752c04b4
Pedro Machado <48498905+pedromchd@users.noreply.github.com>
2023-12-01 18:25:50 +0000

    contact form test

--- contact/tests/test_form_contact.py
+++ contact/tests/test_form_contact.py
@@ -1,1 +1,29 @@
 from django.test import TestCase
+
+from contact.forms import ContactForm
+
+
+class ContactFormTest(TestCase):
+    def test_form_has_fields(self):
+        form = ContactForm()
+        self.assertTupleEqual(tuple(form.fields), ('name', 'email', 'phone', 'message'))
+
+    def test_name_must_be_capitalized(self):
+        form = self.make_validated_form(name='PEDRO machado')
+        self.assertEqual(form.cleaned_data['name'], 'Pedro Machado')
+
+    def test_phone_is_optional(self):
+        form = self.make_validated_form(phone='')
+        self.assertFalse(form.errors)
+
+    def make_validated_form(self, **kwargs):
+        valid = dict(
+            name='Pedro Machado',
+            email='pedro.machado@mail.com',
+            phone='053-98429-5133',
+            message='Lorem ipsum dolor sit amet',
+        )
+        data = dict(valid, **kwargs)
+        form = ContactForm(data)
+        form.is_valid()
+        return form

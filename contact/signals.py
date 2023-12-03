dbac59e7d9ed9de79862eed7035071b773e2026c
Pedro Machado <48498905+pedromchd@users.noreply.github.com>
2023-12-03 14:59:28 +0000

    email de resposta

--- contact/signals.py
+++ contact/signals.py
@@ -1,10 +1,18 @@
+from django.conf import settings
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 from contact.models import Contact
+from contact.views import _send_email as send_email
 
 
 @receiver(post_save, sender=Contact)
 def message_replied(instance, created, update_fields, **kwargs):
     if not created and 'reply' in update_fields:
-        print(instance.reply)
+        send_email(
+            subject='Sua mensagem foi respondida!',
+            from_email=settings.DEFAULT_FROM_EMAIL,
+            recipients=(instance.email, settings.DEFAULT_FROM_EMAIL),
+            template_name='contact/contact_response.txt',
+            context={'response': instance},
+        )
